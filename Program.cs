eb34ce6ca178b2961b823670e1519fe7fcb1af78
toxa90 <anton.lopin00@mail.ru>
2020-02-19 12:50:27 +0300

    Zed

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string connStr = @"Data Source=DESKTOP-IM096BK\SQLEXPRESS;Initial Catalog=Магазин; Integrated Security=True";
+            var conn = new SqlConnection(connStr);
+            conn.Open();
+            
+            var cmd = new SqlCommand("select * from dbo.Поставка", conn);
+            var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine("Дата поступления ");
+            }
+
+            conn.Close();
+
+            Console.Read();
+        }
+    }
+}
