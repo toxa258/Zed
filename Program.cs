480e45313e48c86df868ba3f61977bf956256cb8
toxa90 <anton.lopin00@mail.ru>
2020-03-03 21:52:55 +0300

    https://github.com/toxa258/Zed.git

--- Program.cs
+++ Program.cs
@@ -1,32 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Data.SqlClient;
 
-
-
-
-namespace ConsoleApp11
+namespace ConsoleApp1
 {
     class Program
     {
-
         static void Main(string[] args)
         {
+            string connStr = @"Data Source=DESKTOP-IM096BK\SQLEXPRESS;Initial Catalog=Shop_Test;Integrated Security=True;";
 
-            string connStr = @"Data Source=DESKTOP-IM096BK\SQLEXPRESS;Initial Catalog=Магазин; Integrated Security=True";
-            var conn = new SqlConnection(connStr);
+            System.Data.SqlClient.SqlConnection conn = new System.Data.SqlClient.SqlConnection(connStr);
+
             conn.Open();
-            
-            var cmd = new SqlCommand("select id from dbo.Поставка", conn);
-            var reader = cmd.ExecuteReader();
-
+            string date = Console.ReadLine();
+            Console.WriteLine("Left in stock:");
+            String sql = @"SELECT t.id,Name, (o.Quantity - p.Quantity) Quantity FROM Products t
+                           join Sales p on t.ID = p.idProducts
+                           join Supply o on t.ID = o.idProducts
+                           where o.Date <='" + date + "' and  p.Date <='" + date + "'";
+            SqlCommand command = new SqlCommand(sql, conn);
+            SqlDataReader reader = command.ExecuteReader();
             while (reader.Read())
             {
-                Console.WriteLine("Дата поступления ");
+                Console.WriteLine("{0,18} | {1,11} | {2,5}", reader[0].ToString(), reader[1].ToString(), reader[2].ToString());
             }
-
+            Console.Read();
+            reader.Close();
             conn.Close();
 
-            Console.Read();
         }
     }
 }
